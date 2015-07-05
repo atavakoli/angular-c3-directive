c91666270d78870d235b1a8d8ecc479dd0942a31
Ali Tavakoli <ali.tavakoli@gmail.com>
2015-07-05 07:31:29 -0400

    Added data.hide model detection
    Changes to the data.hide model will now update chart appropriately.
    Conversely, if the model is updated by clicking the legend items, the
    model is updated and a digest is triggered via $apply.
    
    The current implementation makes it impossible for a user to implement their
    own onclick (or for that matter, their own legend). This will be addressed in
    a future release.

--- angular-c3.js
+++ angular-c3.js
@@ -47,20 +47,81 @@ angular.module('c3', [])
     return (delta.length ? delta : false);
   }
 
+  function doHide(chart, data) {
+    var ids;
+    if (data.columns) {
+      ids = data.columns.map(function(d) { return d[0]; });
+    } else if (data.rows) {
+      ids = data.rows[0];
+    }
+
+    var hidden = [];
+    var shown = [];
+    if (!data.hide || !data.hide.length) {
+      ids.forEach(function(id) { shown.push(id); });
+    } else {
+      ids.forEach(function(id) {
+        if (data.hide.indexOf(id) >= 0) {
+          hidden.push(id);
+        } else {
+          shown.push(id);
+        }
+      });
+    }
+
+    if (hidden.length > 0) {
+      chart.hide(hidden);
+    }
+    if (shown.length > 0) {
+      chart.show(shown);
+    }
+
+    // Not exactly sure why this is needed, but without it,
+    // you have legend items stay focused after a hover
+    chart.revert();
+  }
+
+  function toggleData(id, data) {
+    if (data.hide) {
+      var idx = data.hide.indexOf(id);
+      if (idx >= 0) {
+        data.hide.splice(idx, 1);
+      } else {
+        data.hide.push(id);
+      }
+    } else {
+      data.hide = ['id'];
+    }
+  }
+
   return {
     restrict: 'A',
     link: function(scope, elem, attrs) {
       var chart;
       var unwatchFcn = scope.$watch(attrs.c3, function(value, prevValue) {
         if (value) {
-          var config = angular.extend({}, value, { bindto: elem[0] });
+          var config = angular.extend({}, value, {
+            bindto: elem[0],
+            legend: {
+              item: {
+                onclick: function(id) {
+                  if (config.data) {
+                    scope.$apply(function() { toggleData(id, config.data); });
+                  }
+                }
+              }
+            }
+          });
 
           if (!!chart && !!config.data) {
+            // TODO check if data is the only thing changed; if not, then
+            // regenerate the chart rather than call load()
             var unload = getUnload(prevValue.data, config.data);
             if (unload) {
               angular.extend(config.data, {unload: unload});
             }
             chart.load(config.data);
+            doHide(chart, config.data);
           } else {
             chart = c3.generate(config);
           }
