ce11446b1d2655d59686357a415de8170f69430e
Ali Tavakoli <ali.tavakoli@gmail.com>
2015-07-05 05:02:44 -0400

    Added load/unload support for column & row data
    Instead of regenerating the whole chart, it now uses the chart.load API to
    update existing data IDs & add new data IDs. For removed IDs, the previous &
    current data are inspected & the absent IDs are unloaded.
    
    Only data.columns & data.rows supports the unload detection functionality.
    data.json can probably be added without much complexity, but data.url will
    either need unload:true or implementation of the 'done' handler.

--- angular-c3.js
+++ angular-c3.js
@@ -5,27 +5,78 @@ angular.module('c3', [])
 
 .directive('c3', function() {
   console.log('loading directive');
+
+  function getUnload(prev, curr) {
+    var prevKeys;
+    if (prev && prev.columns && prev.columns.length) {
+      prevKeys = {};
+      prev.columns.forEach(function(data) {
+        prevKeys[data[0]] = true;
+      });
+    } else if (prev && prev.rows && prev.rows.length) {
+      prevKeys = {};
+      prev.rows[0].forEach(function(key) {
+        prevKeys[key] = true;
+      });
+    } else {
+      return false;
+    }
+
+    var currKeys;
+    if (curr && curr.columns && curr.columns.length) {
+      currKeys = {};
+      curr.columns.forEach(function(data) {
+        currKeys[data[0]] = true;
+      });
+    } else if (curr && curr.rows && curr.rows.length) {
+      currKeys = {};
+      curr.rows[0].forEach(function(key) {
+        currKeys[key] = true;
+      });
+    } else {
+      return Object.keys(prevKeys);
+    }
+
+    var delta = [];
+    Object.keys(prevKeys).forEach(function(key) {
+      if (!currKeys.hasOwnProperty(key)) {
+        delta.push(key);
+      }
+    });
+
+    return (delta.length ? delta : false);
+  }
+
   return {
     restrict: 'A',
     link: function(scope, elem, attrs) {
       var chart;
-      var unwatchFcn = scope.$watch(attrs.c3, function(value) {
+      var unwatchFcn = scope.$watch(attrs.c3, function(value, prevValue) {
         if (value) {
           var config = angular.extend({}, value, { bindto: elem[0] });
-          chart = c3.generate(config);
+
+          if (!!chart && !!config.data) {
+            var unload = getUnload(prevValue.data, config.data);
+            if (unload) {
+              angular.extend(config.data, {unload: unload});
+            }
+            chart.load(config.data);
+          } else {
+            chart = c3.generate(config);
+          }
         } else {
           if (chart && typeof chart.destroy === 'function') {
             chart = chart.destroy();
           }
         }
       }, true);
-      
+
       elem.on('$destroy', function() {
         if (chart && typeof chart.destroy === 'function') {
           chart = chart.destroy();
         }
       });
-      
+
       scope.$on('$destroy', function() {
         if (chart && typeof chart.destroy === 'function') {
           chart = chart.destroy();
